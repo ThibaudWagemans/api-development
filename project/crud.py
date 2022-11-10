0d87fd1c49198993ebf8cb83ab8196ba92b1f04b
tibogeeraerts <r0882113@student.thomasmore.be>
2022-11-10 15:10:31 +0100

    Make database quotes automatically

--- project/crud.py
+++ project/crud.py
@@ -5,11 +5,46 @@ import models
 import schemas
 
 
+def populate_database(db: Session):
+    quotes = [
+        "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        "The way to get started is to quit talking and begin doing.",
+        "Your time is limited, so don't waste it living someone else's life.",
+        "If life were predictable it would cease to be life, and be without flavor.",
+        "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+        "Life is what happens when you're busy making other plans.",
+        "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+        "When you reach the end of your rope, tie a knot in it and hang on.",
+        "Always remember that you are absolutely unique. Just like everyone else.",
+        "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+        "The future belongs to those who believe in the beauty of their dreams.",
+        "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+        "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+        "It is during our darkest moments that we must focus to see the light.",
+        "Whoever is happy will make others happy too.",
+        "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+        "You will face many defeats in life, but never let yourself be defeated.",
+        "In the end, it's not the years in your life that count. It's the life in your years.",
+        "Never let the fear of striking out keep you from playing the game.",
+        "Life is either a daring adventure or nothing at all.",
+        "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+        "You only live once, but if you do it right, once is enough.",
+        "Many people think of prosperity that concerns money only to forget that true prosperity is of the mind.",
+        "To live a creative life, we must lose our fear of being wrong.",
+        "If you are not willing to risk the usual you will have to settle for the ordinary."]
+    for quote in quotes:
+        db_quote = models.Quote(content=quote)
+        db.add(db_quote)
+        db.commit()
+        db.refresh(db_quote)
+    return 0
+
+
 def get_quote(db: Session, quote_id: int):
     return db.query(models.Quote).filter(models.Quote.id == quote_id).first()
 
 
-def get_quotes(db: Session, skip: int = 0, limit: int = 100):
+def get_quotes(db: Session, skip: int = 0, limit: int = 25):
     return db.query(models.Quote).offset(skip).limit(limit).all()
 
 
--- project/main.py
+++ project/main.py
@@ -24,6 +24,12 @@ def get_db():
     finally:
         db.close()
 
+# populate database with quotes
+@app.on_event("startup")
+def startup_event():
+    db = SessionLocal()
+    crud.populate_database(db)
+
 # Create quote
 @app.post("/quotes/", response_model=schemas.Quote)
 def create_quote(quote: schemas.QuoteCreate, db: Session = Depends(get_db)):
@@ -35,8 +41,17 @@ def read_quotes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     quotes = crud.get_quotes(db, skip=skip, limit=limit)
     return quotes
 
+# GET random quote
 @app.get("/quotes/random", response_model=schemas.Quote)
 def read_quote_random(db: Session = Depends(get_db)):
     quotes = crud.get_quotes(db)
     quote = random.choice(quotes)
+    return quote
+
+
+# GET last quote
+@app.get("/quotes/last", response_model=schemas.Quote)
+def read_quote_last(db: Session = Depends(get_db)):
+    all_quotes = crud.get_quotes(db)
+    quote = all_quotes[-1]
     return quote
