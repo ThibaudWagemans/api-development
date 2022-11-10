e3c570f8f0f6c567be80af46b8fef27c9d72941c
tibogeeraerts <r0882113@student.thomasmore.be>
2022-11-10 15:23:08 +0100

    Update crud.py

--- project/crud.py
+++ project/crud.py
@@ -40,17 +40,17 @@ def populate_database(db: Session):
     return 0
 
 
-def get_quote(db: Session, quote_id: int):
-    return db.query(models.Quote).filter(models.Quote.id == quote_id).first()
-
-
-def get_quotes(db: Session, skip: int = 0, limit: int = 25):
-    return db.query(models.Quote).offset(skip).limit(limit).all()
-
-
 def create_quote(db: Session, quote: schemas.QuoteCreate):
     db_quote = models.Quote(content=quote.content)
     db.add(db_quote)
     db.commit()
     db.refresh(db_quote)
     return db_quote
+
+
+def get_quote(db: Session, quote_id: int):
+    return db.query(models.Quote).filter(models.Quote.id == quote_id).first()
+
+
+def get_quotes(db: Session, skip: int = 0, limit: int = 25):
+    return db.query(models.Quote).offset(skip).limit(limit).all()
