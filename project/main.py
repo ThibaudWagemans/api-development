844e92aa1971370b1dd30ff8cf188b617185ebbc
tibogeeraerts <r0882113@student.thomasmore.be>
2022-11-12 14:10:13 +0100

    TEST CORS in main.py

--- project/main.py
+++ project/main.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 import os
 import random
 
@@ -25,6 +26,18 @@ def get_db():
     finally:
         db.close()
 
+# allowed origins for CORS
+origins = ["*"]
+
+# add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # populate database with quotes on startup
 @app.on_event("startup")
 def startup_event():
@@ -33,25 +46,25 @@ def startup_event():
 
 # Create custom quote
 @app.post("/quotes/", response_model=schemas.Quote)
-def create_quote(quote: schemas.QuoteCreate, db: Session = Depends(get_db)):
+async def create_quote(quote: schemas.QuoteCreate, db: Session = Depends(get_db)):
     new_quote = crud.create_quote(db=db, quote=quote)
     return new_quote
 
 # GET all quotes
 @app.get("/quotes/all", response_model=list[schemas.Quote])
-def read_quotes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_quotes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     quotes = crud.get_quotes(db, skip=skip, limit=limit)
     return quotes
 
 # GET random quote
 @app.get("/quotes/random", response_model=schemas.Quote)
-def read_quote_random(db: Session = Depends(get_db)):
+async def read_quote_random(db: Session = Depends(get_db)):
     quotes = crud.get_quotes(db)
     quote = random.choice(quotes)
     return quote
 
 # GET last quote
 @app.get("/quotes/last", response_model=schemas.Quote)
-def read_quote_last(db: Session = Depends(get_db)):
+async def read_quote_last(db: Session = Depends(get_db)):
     quote = crud.get_quote_last(db)
     return quote
