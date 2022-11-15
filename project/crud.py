05ece6168bae01f084ce1320a79fb15a9bbd4633
tibogeeraerts <r0882113@student.thomasmore.be>
2022-11-15 16:17:05 +0100

    Fix typo in comments

--- project/crud.py
+++ project/crud.py
@@ -34,7 +34,7 @@ def get_quote(db: Session, quote_id: int):
     quote_by_id = db.query(models.Quote).filter(models.Quote.id == quote_id).first()
     return quote_by_id
 
-# get all quotes (max 25)
+# get all quotes (max 50)
 def get_quotes(db: Session, skip: int = 0, limit: int = 50):
     all_quotes = db.query(models.Quote).offset(skip).limit(limit).all()
     return all_quotes
