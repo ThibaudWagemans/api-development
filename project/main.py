9dd1c8327d8d742015265fdf87c76f78ec84769f
tibogeeraerts <r0882113@student.thomasmore.be>
2022-11-17 14:19:00 +0100

    Change CORS values

--- project/main.py
+++ project/main.py
@@ -31,7 +31,8 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "https://localhost.tiangolo.com",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "https://geeraertstibo-api.netlify.app/"
 ]
 
 # add CORS middleware
